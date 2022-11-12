a6e91ef377c5762f3dc0d868b4aa1e0b63bdd3c4
Rashmi <patel25rashmi@gmail.com>
2022-11-12 18:19:08 +0530

    import navbar

---
+++ components/navbarAdmin.js
@@ -0,0 +1,26 @@
+const navbar2 = () => {
+    return `
+    <div>
+    <div id="logo">
+        <img src="./images/logo.png" alt="logo">
+    </div>
+</div>
+
+<div id="sellerD">
+    <h3><a href="adminHome.html">Seller Dashboard</a></h3>
+</div>
+
+ <div id="help">
+    <h4>Help : royal@elec.com</h4>
+    <h4>Contact us : 9384285012</span></h4>
+</div>
+
+<div id="signinup">
+    <h4><a href="signin.html">SignIn</a></h4>
+    <h4><a href="signup.html">SignUp</a></h4>
+</div>
+    `
+};
+
+
+export { navbar2 };
